051d8a2b3acc5d5eb42e0d93c1dd21955b2e4da6
juan <juansvrs0321@gmail.com>
2024-05-05 15:28:02 -0500

    Pagina informes ready

--- src/app/feature/informes/informes.component.ts
+++ src/app/feature/informes/informes.component.ts
@@ -3,27 +3,98 @@ import { RegistroService } from '../registros/service/registro.service';
 import { Registro } from '../registros/service/registro';
 import { RegistroComponent } from '../registros/registro/registro.component';
 import { CommonModule } from '@angular/common';
+import { PacienteService } from '../pacientes/service/paciente.service';
+import { ActividadService } from '../actividades/service/actividad.service';
+import { FormsModule } from '@angular/forms';
 @Component({
-  selector: 'app-informes',
-  standalone: true,
-  imports:[RegistroComponent,CommonModule],
-  templateUrl: './informes.component.html',
-  styleUrl: './informes.component.css'
+    selector: 'app-informes',
+    standalone: true,
+    templateUrl: './informes.component.html',
+    styleUrl: './informes.component.css',
+    imports: [RegistroComponent, CommonModule,FormsModule]
 })
-export class InformesComponent {
-  listadoRegistros: any[] = []; // Variable para almacenar los registros
 
-  constructor(private registroService: RegistroService) {} // Inyecta el servicio de registros
+export class InformesComponent {
+  listadoRegistros: Registro[] = [];
+  pacientes: any[] = [];
+  actividades: any[] = [];
+
+  pacienteId: string = '';
+  actividadId: number | null = null;
+
+  constructor(
+    private registroService: RegistroService,
+    private pacienteService: PacienteService,
+    private actividadService: ActividadService
+  ) {}
 
   cargarRegistros() {
-    // Llama al servicio para obtener todos los registros
-    this.registroService.getAllRegistros().subscribe(
-      (registros: any[]) => {
-        this.listadoRegistros = registros; // Guarda los registros en la variable listadoRegistros
+    if (!this.pacienteId && !this.actividadId) {
+      // Si ambos campos están vacíos, cargar todos los registros
+      this.registroService.getAllRegistros().subscribe(
+        (registros: Registro[]) => {
+          this.listadoRegistros = registros;
+        },
+        error => {
+          console.error('Error al obtener los registros:', error);
+        }
+      );
+    } else if (this.pacienteId && !this.actividadId) {
+      // Solo paciente seleccionado
+      this.registroService.getAllRegistrosByPaciente(this.pacienteId).subscribe(
+        (registros: Registro[]) => {
+          this.listadoRegistros = registros;
+        },
+        error => {
+          console.error('Error al obtener los registros:', error);
+        }
+      );
+    } else if (!this.pacienteId && this.actividadId) {
+      // Solo actividad seleccionada
+      this.registroService.getAllRegistrosByActividad(this.actividadId).subscribe(
+        (registros: Registro[]) => {
+          this.listadoRegistros = registros;
+        },
+        error => {
+          console.error('Error al obtener los registros:', error);
+        }
+      );
+    } else if (this.pacienteId && this.actividadId) {
+      // Ambos paciente y actividad seleccionados
+      this.registroService.getAllRegistrosByPacienteAndActividad(this.pacienteId, this.actividadId).subscribe(
+        (registros: Registro[]) => {
+          this.listadoRegistros = registros;
+        },
+        error => {
+          console.error('Error al obtener los registros:', error);
+        }
+      );
+    }
+  }
+
+  cargarPacientes() {
+    this.pacienteService.getAllPacientes().subscribe(
+      (pacientes: any[]) => {
+        this.pacientes = pacientes;
       },
       error => {
-        console.error('Error al obtener los registros:', error);
+        console.error('Error al obtener los pacientes:', error);
       }
     );
+  }
+
+  cargarActividades() {
+    this.actividadService.getAllActividades().subscribe(
+      (actividades: any[]) => {
+        this.actividades = actividades;
+      },
+      error => {
+        console.error('Error al obtener las actividades:', error);
+      }
+    );
+  }
+
+  esRegistroRealizado(registro: Registro): boolean {
+    return registro.estado === 'Realizado';
   }
 }
